25f493e2946571f365c2b6bf8900d169f91c900b
Lizzie Perez <lizziejperez@gmail.com>
2021-10-19 18:21:30 -0400

    Update concat.s

--- Project-4/concat.s
+++ Project-4/concat.s
@@ -1,11 +1,20 @@
-main:
-    ;mov r3, &"hello"
-    mov r2, #5
-    ;mov r1, &"goodbye"
-    mov r0, #7
-    add r4, r2, r0
-    mov r5, #1
-    add r4, r4, r5
-    bl allocate
-allocate:
-    
+main
+		mov		r3, #hello
+		mov		r2, #5
+		mov		r1, #goodbye
+		mov		r0, #7
+		;		Result
+		mov		r4, #0
+		add		r4, r4, r2
+		add		r4, r4, r0
+		add		r4, r4, #1
+		bl		ALLOC
+		mov		r4, #0
+		;		While loops
+		end
+
+ALLOC
+		
+
+hello	DCD		104, 101, 108, 108, 111, 0
+goodbye	DCD		103, 111, 111, 100, 98, 121, 101, 0
