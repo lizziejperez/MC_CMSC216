14a7ce25f9ea4ce45a966d87ae1e31fde595e8d4
Lizzie Perez <lizziejperez@gmail.com>
2021-10-20 23:24:03 -0400

    Update concat.s

--- Project-4/concat.s
+++ Project-4/concat.s
@@ -1,5 +1,12 @@
 main
+		;		fp(r11-b) and sp(r13-t) setup
+		str		r11, [r13, #-4]!
+		;add		r11, r13, #0
+		sub		r13, r13, #24
+		str		LR, [fp, #-8
+		;		vars setup
 		mov		r3, #hello
+		;ldrb	r5, [r3,#4] second letter
 		mov		r2, #5
 		mov		r1, #goodbye
 		mov		r0, #7
@@ -11,10 +18,15 @@ main
 		bl		ALLOC
 		mov		r4, #0
 		;		While loops
+		;		fp and sp clean up
+		add		sp, sp, #4
+		ldr		fp, [sp]
+		mov		LR, PC
+		sub		r13, r11, #0
+		ldr		r11, [r13, #4]
 		end
-
+		
 ALLOC
 		
-
 hello	DCD		104, 101, 108, 108, 111, 0
 goodbye	DCD		103, 111, 111, 100, 98, 121, 101, 0
