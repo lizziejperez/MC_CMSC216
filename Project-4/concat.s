e3ca0bed5ab2474bbe0f9922fe9c3b5fa15ab686
Lizzie Perez <lizziejperez@gmail.com>
2021-10-23 01:07:15 -0400

    Update concat.s

--- Project-4/concat.s
+++ Project-4/concat.s
@@ -14,8 +14,12 @@ main
 		bl		_allocate
 		ldr		r0, [r13, #-4]
 
-		;		While loops (ldrb	r5, [r3,#4] second letter)
-
+		; 1 - while *s1 != #0
+		cmp		r3, #0
+		blt		whileS1
+		; 1 - while *s1 != #0
+		cmp		r1, #0
+		blt		whileS2
 		;		clean up
 		add		r13, r13, #24
 		ldr		r11, [r13, #4]
@@ -23,6 +27,12 @@ main
 
 _allocate
 		sub		r13, r13, r0
+
+whileS1
+		; goes thru each character in s1 (r3) and adds it into result at index i (r?)
+		; the index i and s1 are incrementing each iteration
+		; note: ldrb	r5, [r3,#4] second letter
+whileS2
 
 hello	DCD		104, 101, 108, 108, 111, 0
 ;hello DCD		0x 00 6F 6C 6C 65 68
