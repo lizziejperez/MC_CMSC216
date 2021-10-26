9b4cf874eed41bed2c74449ca8b0d16c533f6a89
Lizzie Perez <lizziejperez@gmail.com>
2021-10-26 19:24:37 -0400

    Update utils.s

--- Project-5/utils.s
+++ Project-5/utils.s
@@ -17,9 +17,57 @@
 ;			
 ;HELLO		DCD		1819043144,1461726319,1684828783,33
 ;TEST		DCD		1936287828,544434464,1702109281,3044467
-		
-		
-			
+
+; Name: Elizabeth Perez
+; Student ID: eperez57
+; M-number: M20966722
+_POST2INFIX
+		STMFD	SP!, {R14,R11}
+		MOV		R11, SP
+		SUB		SP, SP, #16
+		STR		R0, [R11, #-4] ; storing input string at FP-4
+		BL		_STRLEN
+		STR		R0, [R11, #-8] ; storing length of input string at FP-8
+		MOV		R1, #0
+		STR		R1, [R11, #-12] ; storing index counter at FP-12
+_POST2INFIX_L1 ; checks if index < input.length
+		LDR		R0, [R11, #-4] ; loads input string to R0
+		LDR		R1, [R11, #-8] ; loads length of input string to R1
+		LDR		R2, [R11, #-12] ; loads index counter to R2
+		CMP		R2, R1
+		BGE		_POST2INFIX_L6
+_POST2INFIX_L2 ; gets the char, then increases index counter by 1
+		LDRB		R1, [R0], #R2 ; loads char to R2
+		ADD		R2, R2, #1
+		STR		R2, [R11, #-12] ; updating index counter
+_POST2INFIX_L3 ; if the charater is blank, skip it
+		CMP		R1, #32
+		BEQ		_POST2INFIX_L1
+_POST2INFIX_L4 ; if the character is a digit, put in onto the stack
+		CMP		R1, #48
+		BLT		_POST2INFIX_L5
+		CMP		R1, #57
+		BGT		_POST2INFIX_L5
+		; pushes the character onto the stack
+		MOV		R0, R1
+		BL		_PUSH
+		B		_POST2INFIX_L1
+_POST2INFIX_L5 ; it is an operator
+		;	(a) pop the term2 off the stack
+		;	(b) pop the term1 off the stack
+		;	(c) concatenate: '('+term1+operator+term2+')'
+		;	(d) push the concatenated string onto the stack
+_POST2INFIX_L6 ; at the end of the input string, if there is only one thing on the stack, it is converted infix formulation of the expression
+		BL		_STACK_SIZE
+		CMP		R0, #1
+		BNE		???
+		_POP
+
+_POST2INFIX_RTN
+		ADD		SP, SP, #16
+		LDMFD	SP!, {R14,R11}
+		MOVEQ	R15, R14
+
 ; ******* Subroutines begin here *******		
 
 ; returns the length of string passed into R0
