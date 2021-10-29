a4819635a20c3f178cb421e486a0c0884f4a2ad1
Lizzie Perez <lizziejperez@gmail.com>
2021-10-29 19:52:08 -0400

    Update post2infix.s

--- Project-5/post2infix.s
+++ Project-5/post2infix.s
@@ -1,6 +1,90 @@
 ; Name: Elizabeth Perez
 ; Student ID: eperez57
 ; M-number: M20966722
+
+_POST2INFIX
+		STMFD	SP!, {R14,R11}
+		MOV		R11, SP
+		SUB		SP, SP, #24
+		STR		R0, [R11, #-4] ; storing input (a string address) at FP-4
+		MOV		R0, #0
+		STR		R0, [R11, #-8] ; storing index counter at FP-8
+_POST2INFIX_L1
+		LDR		R0, [R11, #-4] ; loads input string to R0
+		LDR		R1, [R11, #-8] ; loads index counter to R1
+		LDRB		R2, [R0, R1] ; loads the charater (input[index]) to R2
+		CMP		R2, #0
+		BEQ		_POST2INFIX_RTN ; if the charater is null, branch to _POST2INFIX_RTN
+		CMP		R2, #32
+		BEQ		_POST2INFIX_L4 ; if the charater is blank, branch to _POST2INFIX_L4
+		STR		R2, [R11, #-12] ; stores the character at FP-12
+		MOV		R0, R2
+		BL		_POST2INFIX_CHARPTR
+		LDR		R2, [R11, #-12] ; restores the character to R2
+		CMP		R2, #48
+		BLT		_POST2INFIX_L2 ; if the character is less than '0', branch to _POST2INFIX_L2
+		CMP		R2, #57
+		BGT		_POST2INFIX_L2 ; if the character is less than '9', branch to _POST2INFIX_L2
+		BL		_PUSH ; the charater is a digit - push onto stack
+		B		_POST2INFIX_L4
+_POST2INFIX_L2
+		STR		R0, [R11, #-12] ; storing the operator at FP-12
+		LDR		R0, [R11, #-16]
+		CMP		R0, #0
+		BNE		_POST2INFIX_L3
+		MOV		R0, #40
+		BL		_POST2INFIX_CHARPTR
+		STR		R0, [R11, #-16] ; storing '(' at FP-16
+		MOV		R0, #41
+		BL		_POST2INFIX_CHARPTR
+		STR		R0, [R11, #-20] ; storing ')' at FP-20
+_POST2INFIX_L3
+		BL		_POP ; (a) pop the term2 off the stack
+		STR		R0, [R11, #-24] ; storing term2 at FP-24
+		BL		_POP ; (b) pop the term1 off the stack
+		MOV		R1, R0
+		LDR		R0, [R11, #-16]
+		BL		_CONCAT ; '('+term1
+		LDR		R1, [R11, #-12]
+		BL		_CONCAT ; '('+term1+operator
+		LDR		R1, [R11, #-24]
+		BL		_CONCAT ; '('+term1+operator+term2
+		LDR		R1, [R11, #-20]
+		BL		_CONCAT ; '('+term1+operator+term2+')'
+		BL		_PUSH ; push the concatenated string onto the stack
+_POST2INFIX_L4
+		LDR		R1, [R11, #-8] ; loads index counter to R1
+		ADD		R1, R1, #1
+		STR		R1, [R11, #-8] ; increases index counter by 1
+		B		_POST2INFIX_L1
+_POST2INFIX_CHARPTR
+		STMFD	SP!, {R14,R11}
+		MOV		R11, SP
+		SUB		SP, SP, #4
+		STR		R0, [R11, #-4]
+		MOV		R0, #2
+		BL		_ALLOC
+		CMP		R0, #0
+		BEQ		_POST2INFIX_ERR ; if _ALLOC fails, return error
+		LDR		R1, [R11, #-4]
+		STRB		R1, [R0]
+		ADD		SP, SP, #4
+		LDMFD	SP!, {R14,R11}
+		MOV	R15, R14
+_POST2INFIX_ERR
+		MOV		R0, #0
+		B		_POST2INFIX_END
+_POST2INFIX_RTN
+		BL		_STACK_SIZE
+		CMP		R0, #1
+		BNE		_POST2INFIX_ERR ; if there is not one thing on the stack, return error
+		BL		_POP
+_POST2INFIX_END
+		ADD		SP, SP, #24
+		LDMFD	SP!, {R14,R11}
+		MOV	R15, R14
+
+; ********************************************************
 _POST2INFIX ; setup
 		STMFD	SP!, {R14,R11}
 		MOV		R11, SP
