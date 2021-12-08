d02880034b0ca8f1bf88f3b1dccec6e907b5b5fb
Lizzie Perez <lizziejperez@gmail.com>
2021-12-07 20:49:54 -0500

    Update MTHash.c

--- Project-7/MTHash.c
+++ Project-7/MTHash.c
@@ -9,11 +9,9 @@ typedef struct _Data {
 } Data;
 
 typedef struct _HashNode {
-    // int id;
 	Data *dataBucket;
     pthread_mutex_t *locker;
 	struct _HashNode *next;
-	struct _HashNode *prev;
 } HashNode;
 
 typedef struct _mt_hashtable {
@@ -29,21 +27,17 @@ MTHash *MTH_init(int tsize) {
     while(hashTable->tsize < tsize) {
         if(hashTable->head == NULL) {
             hashTable->head = malloc(sizeof(HashNode));
-            // hashTable->head->id = hashTable->tsize;
             hashTable->head->dataBucket = NULL;
             pthread_mutex_init(hashTable->head->locker, NULL);
             hashTable->head->next = NULL;
-            hashTable->head->prev = NULL;
         } else {
             HashNode *ptr = hashTable->head;
             while(ptr->next != NULL)
                 ptr = ptr->next;
             ptr->next = malloc(sizeof(HashNode));
-            // ptr->next->id = hashTable->tsize;
             ptr->next->dataBucket = NULL;
             pthread_mutex_init(ptr->next->locker, NULL);
             ptr->next->next = NULL;
-            ptr->next->prev = ptr;
         }
         hashTable->tsize++;
     }
@@ -58,39 +52,113 @@ int MTH_add(MTHash *hashTable, int value) {
         ptr = ptr->next;
     }
 
+    pthread_mutex_lock(ptr->locker);
     if(ptr->dataBucket == NULL) {
-        pthread_mutex_lock(ptr->locker);
         ptr->dataBucket = malloc(sizeof(Data));
         ptr->dataBucket->data = value;
         ptr->dataBucket->next = NULL;
         pthread_mutex_unlock(ptr->locker);
-    } else {
-        if(ptr->dataBucket->data == value) return -1;
-
-        Data *ptr2 = ptr->dataBucket;
-        while(ptr2->next != NULL) {
-            ptr2 = ptr2->next;
-            if(ptr2->data == value) return -1;
-        }
-
-        pthread_mutex_lock(ptr->locker);
-        ptr2->next = malloc(sizeof(Data));
-        ptr2->next->data = value;
-        ptr2->next->next = NULL;
-        pthread_mutex_unlock(ptr->locker);
+        return 0;
     }
-    
 
+    if(ptr->dataBucket->data == value) {
+        pthread_mutex_unlock(ptr->locker);
+        return -1;
+    }
+
+    Data *ptr2 = ptr->dataBucket;
+    while(ptr2->next != NULL) {
+        ptr2 = ptr2->next;
+        if(ptr2->data == value) {
+            pthread_mutex_unlock(ptr->locker);
+            return -1;
+        }
+    }
+    ptr2->next = malloc(sizeof(Data));
+    ptr2->next->data = value;
+    ptr2->next->next = NULL;   
+    pthread_mutex_unlock(ptr->locker);
     return 0;
 }
 
 int MTH_delete(MTHash *hashTable, int value) {
     int hashResult = value % hashTable->tsize;
-    HashNode *ptr = hashTable->head;
+    HashNode *ptr = hashTable->head; /* Bucket pointer */
     for(int i = 0; i < hashResult; i++) {
         ptr = ptr->next;
     }
 
-    if(ptr->dataBucket == NULL) return -1;
-    
+    pthread_mutex_lock(ptr->locker);
+    if(ptr->dataBucket == NULL) { /* Case: No data in the bucket */
+        pthread_mutex_unlock(ptr->locker);
+        return -1;
+    }
+
+    if(ptr->dataBucket->data == value) { /* Case: Delete head data */
+        Data *next = ptr->dataBucket->next;
+        free(ptr->dataBucket);
+        ptr->dataBucket = next;
+        pthread_mutex_unlock(ptr->locker);
+        return 0;
+    }
+
+    Data *ptr2 = ptr->dataBucket; /* Parent data pointer */
+    while(ptr2->next != NULL) {
+        if(ptr2->next->data == value) { /* Case: Delete non-head data */
+            Data *next = ptr2->next->next;
+            free(ptr2->next);
+            ptr2->next = next;
+            pthread_mutex_unlock(ptr->locker);
+            return 0;
+        }
+        ptr2 = ptr2->next;
+    }
+
+    pthread_mutex_unlock(ptr->locker);
+    return -1;
+}
+
+int MTH_size(MTHash *hashTable) {
+    int count = 0;
+    for(int i = 0; i < hashTable->tsize; i++) {
+        count += MTH_bucket_size(hashTable, i);
+    }
+    return count;
+}
+
+int MTH_bucket_size(MTHash *hashTable, int index) {
+    HashNode *ptr = hashTable->head;
+    for(int i = 0; i < index; i++) {
+        ptr = ptr->next;
+    }
+
+    if(ptr->dataBucket == NULL) return 0;
+
+    int count = 1;
+    pthread_mutex_lock(ptr->locker);
+    Data *ptr2 = ptr->dataBucket;
+    while(ptr2->next != NULL) {
+        count++;
+        ptr2 = ptr2->next;
+    }
+    pthread_mutex_unlock(ptr->locker);
+    return count;
+}
+
+void MTH_print(MTHash *hashTable) {
+    HashNode *ptr = hashTable->head;
+    for(int i = 0; i < hashTable->tsize; i++) {        
+        pthread_mutex_lock(ptr->locker);
+        Data *ptr2 = ptr->dataBucket;
+        if(ptr2 != NULL) {
+            printf("%d", ptr2->data);
+            while(ptr2->next != NULL) {
+                ptr2 = ptr2->next;
+                printf(", %d", ptr2->data);
+            }
+        }
+        printf("\n");
+        pthread_mutex_unlock(ptr->locker);
+        ptr = ptr->next;
+    }
 }
