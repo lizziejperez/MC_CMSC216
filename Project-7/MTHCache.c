f53f46df463b3cc6415037f26c0261a62c3bb56c
Lizzie Perez <lizziejperez@gmail.com>
2021-12-11 17:34:16 -0500

    Update MTHCache.c

--- Project-7/MTHCache.c
+++ Project-7/MTHCache.c
@@ -27,7 +27,7 @@ MTCache *MTL_init(int size) {
 }
 
 int MTL_add(MTCache *cache, int value) {
-    if(value <= 0) return -1;
+    if(value <= 0) return -1; /* Case: Invalid value given */
 
     pthread_mutex_lock(cache->locker);
     if(cache->mru == NULL) { /* Case: First entry */
@@ -82,13 +82,13 @@ int MTL_add(MTCache *cache, int value) {
 }
 
 int MTL_update(MTCache *cache, int value) {
-    if(cache->mru == NULL) return -1;
-    if(cache->mru->id == value) return 0;
+    if(cache->mru == NULL) return -1; /* Case: Empty cache */
+    if(cache->mru->id == value) return 0; /* Case: Already mru */
 
     pthread_mutex_lock(cache->locker);
     Node *curr = cache->mru;
     while(curr->next != NULL) {
-        if(curr->next->id == value) {
+        if(curr->next->id == value) { /* Case: Update value to be mru */
             Node *rem = curr->next->next;
             curr->next->prev = NULL;
             curr->next->next = cache->mru;
@@ -101,17 +101,58 @@ int MTL_update(MTCache *cache, int value) {
         curr = curr->next;
     }
     pthread_mutex_unlock(cache->locker);
-    return -1;
+    return -1; /* Case: Value not found in cache */
 }
 
 int MTL_delete(MTCache *cache, int value) {
+    if(cache->mru == NULL) return -1; /* Case: Empty cache */
 
+    pthread_mutex_lock(cache->locker);
+    if(cache->mru->id == value) { /* Case: Delete the mru node */
+        Node *rem = cache->mru->next;
+        free(cache->mru);
+        cache->mru = rem;
+        cache->mru->prev = NULL;
+        cache->used--;
+        pthread_mutex_unlock(cache->locker);
+        return 0;
+    }
+
+    Node *curr = cache->mru;
+    while(curr->next != NULL) {
+        if(curr->next->id == value) { /* Case: Delete "middle" or end node */
+            /* TODO */
+            cache->used--;
+            pthread_mutex_unlock(cache->locker);
+            return 0;
+        }
+    }
+    pthread_mutex_unlock(cache->locker);
+    return -1; /* Case: Value not found in cache */
 }
 
 int MTL_size(MTCache *cache) {
-
+    return cache->used;
+    /*
+    int used = 0;
+    Node *curr = cache->mru;
+    while(curr->next != NULL) {
+        count++;
+        curr = curr->next;
+    }
+    return count:
+    */
 }
 
 void MTH_print(MTCache *cache) {
-
+    pthread_mutex_lock(cache->locker);
+    Node *curr = cache->mru;
+    while(curr != NULL) {
+        printf("%d", curr->id);
+        curr = curr->next;
+        if(curr != NULL) printf(" ");
+    }
+    pthread_mutex_unlock(cache->locker);
+    printf("\n");
+    return 0;
 }
